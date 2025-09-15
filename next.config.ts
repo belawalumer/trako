ff2b5845cd74efcf5fd7b8cc711d27ced0da7ac4
Belawal Umer <41009882+belawalumer@users.noreply.github.com>
2025-09-16 00:47:57 +0500

    skip lint

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  // Skip ESLint during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
